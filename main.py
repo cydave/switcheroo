7c53b27120de0f35d33b10a9d96a3f7f6195b820
dave <dave@cyllective.com>
2020-07-24 12:37:57 +0200

    wip

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from switcheroo.config import Config
 logger, listener = setup_logging()
 
 
-async def start_server(args):
-    logger.info(f"switcheroo listening on %s:%d", args.host, args.port)
+async def start_server():
+    logger.info(f"switcheroo listening on %s:%d", Config.HOST, Config.PORT)
     await asyncssh.create_server(
         server_factory=Config.get_server_class(),
         host=Config.HOST,
@@ -22,23 +22,16 @@ async def start_server(args):
     )
 
 
-def main(args):
+def main():
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(start_server(args))
+        loop.run_until_complete(start_server())
     except (OSError, asyncssh.Error) as exc:
         sys.exit("Error starting server: " + str(exc))
 
     loop.run_forever()
+    listener.stop()
 
 
 if __name__ == "__main__":
-    ap = argparse.ArgumentParser()
-    host = ap.add_argument("-b", "--host", required=False, default="0.0.0.0")
-    port = ap.add_argument("-p", "--port", type=int, required=False, default=10022)
-    version = ap.add_argument(
-        "--server-version", required=False, default="SSH-2.0-OpenSSH_7.4"
-    )
-    args = ap.parse_args()
-    main(args)
-    listener.stop()
+    main()
