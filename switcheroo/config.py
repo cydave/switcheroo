72c9242765d96d4a31eb14513d32a67fb9a26c16
dave <dave@cyllective.com>
2020-07-24 19:38:23 +0200

    wip

--- switcheroo/config.py
+++ switcheroo/config.py
@@ -9,7 +9,7 @@ KEYS_DIR = os.path.join(ROOT_DIR, "keys")
 class Config:
     HOST = os.getenv("SW_HOST", "127.0.0.1")
     PORT = int(os.getenv("SW_PORT", 10022))
-    BANNER = os.getenv("SW_BANNER", "SSH-2.0-OpenSSH_7.4")
+    BANNER = os.getenv("SW_BANNER", "SSH-2.0-OpenSSH_7.4").replace("SSH-2.0-", "")
     DATABASE_URI = os.getenv("SW_DATABASE_URI", "./brain.db")
     SERVER_CLASS = os.getenv("SW_SERVER_TYPE", "SwitcherooSSHServer")
     HOST_KEYS = [
